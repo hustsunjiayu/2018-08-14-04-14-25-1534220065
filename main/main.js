9f11a17aae6c595fec1b5a9127cbd29fef37051b
hustsunjiayu <452696099@qq.com>
2019-05-23 16:12:50 +0800

    wrote the main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,32 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    let list =[
+        ['._.','|.|','|_|'],//0
+        ['...','..|','..|'],//1
+        ['._.','._|','|_.'],//2
+        ['._.','._|','._|'],//3
+        ['...','|_|','..|'],//4
+        ['._.','|_.','._|'],//5
+        ['._.','|_.','|_|'],//6
+        ['._.','..|','..|'],//7
+        ['._.','|_|','|_|'],//8
+        ['._.','|_|','..|'],//9
+    ];
+	var res = "";
+	let str2Num = [];
+	for(i=0;i<str.length;i++)
+		str2Num.push(parseInt(str[i]));
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<str.length;j++)
+		{
+			res += list[str2Num[j]][i];
+			if(j<str.length-1)
+				res += " ";
+			else
+				res += "\n";
+		}
+		
+	}
+	
+    return res;
 };
